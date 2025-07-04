17271804e43909736c8ac80f1cab82c0c71a7e92
Ajinkya Dongare <ajinkya@lealsolution.com>
2025-07-04 14:55:27 +0530

    index page data is replaced, page is almost done, now woek on about page

--- rrengineers/wp-content/themes/rrengineers/footer.php
+++ rrengineers/wp-content/themes/rrengineers/footer.php
@@ -16,17 +16,19 @@
         <div class="row g-5">
             <div class="col-lg-6 pe-lg-5">
                 <a href="index.html" class="navbar-brand">
-                    <h1 class="m-0 display-4 text-uppercase text-white"><i class="bi bi-building text-primary me-2"></i>WEBUILD</h1>
+                    <h1 class="m-0 display-4 text-uppercase text-white">
+                        <i class="bi bi-building text-primary me-2"></i>RR Engineers
+                    </h1>
                 </a>
-                <p>Aliquyam sed elitr elitr erat sed diam ipsum eirmod eos lorem nonumy. Tempor sea ipsum diam  sed clita dolore eos dolores magna erat dolore sed stet justo et dolor.</p>
-                <p><i class="fa fa-map-marker-alt me-2"></i>123 Street, New York, USA</p>
-                <p><i class="fa fa-phone-alt me-2"></i>+012 345 67890</p>
-                <p><i class="fa fa-envelope me-2"></i>info@example.com</p>
+                <p>Established in the year 2011, RR Engineers & Contractors is a leading firm offering Pre-Insulated Ducting Systems, Eco Panels, Spiral Ducting, CNC Machines, Fabrication Tools, and Ducting Accessories. Known for quality, durability, and precision, our products serve the needs of commercial, industrial, and residential projects.</p>
+                <p><i class="fa fa-map-marker-alt me-2"></i>NO-2, Shree Ganesh Vastu, Ashiyana Park-2, Opp. Gaikwad Petrol Pump, Aundh, Pune – 411007, Maharashtra, India</p>
+                <p><i class="fa fa-phone-alt me-2"></i>+91 9421677901, 9881301270</p>
+                <p><i class="fa fa-envelope me-2"></i>sales@rrengineers.in</p>
                 <div class="d-flex justify-content-start mt-4">
-                    <a class="btn btn-lg btn-primary btn-lg-square rounded-0 me-2" href="#"><i class="fab fa-twitter"></i></a>
                     <a class="btn btn-lg btn-primary btn-lg-square rounded-0 me-2" href="#"><i class="fab fa-facebook-f"></i></a>
+                    <a class="btn btn-lg btn-primary btn-lg-square rounded-0 me-2" href="#"><i class="fab fa-youtube"></i></a>
                     <a class="btn btn-lg btn-primary btn-lg-square rounded-0 me-2" href="#"><i class="fab fa-linkedin-in"></i></a>
-                    <a class="btn btn-lg btn-primary btn-lg-square rounded-0" href="#"><i class="fab fa-instagram"></i></a>
+                    <a class="btn btn-lg btn-primary btn-lg-square rounded-0" href="#"><i class="fab fa-twitter"></i></a>
                 </div>
             </div>
             <div class="col-lg-6 ps-lg-5">
@@ -35,27 +37,29 @@
                         <h4 class="text-white text-uppercase mb-4">Quick Links</h4>
                         <div class="d-flex flex-column justify-content-start">
                             <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Home</a>
-                            <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right me-2"></i>About Us</a>
-                            <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Our Services</a>
-                            <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Meet The Team</a>
-                            <a class="text-white-50" href="#"><i class="fa fa-angle-right me-2"></i>Contact Us</a>
+                            <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right me-2"></i>About</a>
+                            <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Services</a>
+                            <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Evaporative Cooling</a>
+                            <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Pal Ducting Material</a>
+                            <a class="text-white-50" href="#"><i class="fa fa-angle-right me-2"></i>Contact</a>
                         </div>
                     </div>
                     <div class="col-sm-6">
-                        <h4 class="text-white text-uppercase mb-4">Popular Links</h4>
+                        <h4 class="text-white text-uppercase mb-4">Other Products</h4>
                         <div class="d-flex flex-column justify-content-start">
-                            <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Home</a>
-                            <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right me-2"></i>About Us</a>
-                            <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Our Services</a>
-                            <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Meet The Team</a>
-                            <a class="text-white-50" href="#"><i class="fa fa-angle-right me-2"></i>Contact Us</a>
+                            <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right me-2"></i>CNC Machine</a>
+                            <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Eco Panels</a>
+                            <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Spiral Ducting</a>
+                            <a class="text-white-50 mb-2" href="#"><i class="fa fa-angle-right me-2"></i>Fabrication Tools</a>
+                            <a class="text-white-50" href="#"><i class="fa fa-angle-right me-2"></i>Ducting Accessories</a>
                         </div>
                     </div>
                     <div class="col-sm-12">
                         <h4 class="text-white text-uppercase mb-4">Newsletter</h4>
                         <div class="w-100">
                             <div class="input-group">
-                                <input type="text" class="form-control border-light" style="padding: 20px 30px;" placeholder="Your Email Address"><button class="btn btn-primary px-4">Sign Up</button>
+                                <input type="text" class="form-control border-light" style="padding: 20px 30px;" placeholder="Your Email Address">
+                                <button class="btn btn-primary px-4">Sign Up</button>
                             </div>
                         </div>
                     </div>
@@ -66,14 +70,15 @@
     <div class="container-fluid bg-dark bg-light-radial text-white border-top border-primary px-0">
         <div class="d-flex flex-column flex-md-row justify-content-between">
             <div class="py-4 px-5 text-center text-md-start">
-                <p class="mb-0">&copy; <a class="text-primary" href="#">Your Site Name</a>. All Rights Reserved.</p>
+                <p class="mb-0">&copy; <a class="text-primary" href="#">RR Engineers & Contractors</a>. All Rights Reserved.</p>
             </div>
             <div class="py-4 px-5 bg-primary footer-shape position-relative text-center text-md-end">
-                <p class="mb-0">Designed by <a class="text-dark" href="https://htmlcodex.com">HTML Codex</a></p>
+                <p class="mb-0">Designed by <a class="text-dark" href="https://lealsolution.com/" target="_blank">Leal Software Solution Pvt Ltd</a></p>
             </div>
         </div>
     </div>
     <!-- Footer End -->
+
 
 
     <!-- Back to Top -->
@@ -83,17 +88,17 @@
     <!-- JavaScript Libraries -->
     <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="lib/easing/easing.min.js"></script>
-    <script src="lib/waypoints/waypoints.min.js"></script>
-    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
-    <script src="lib/tempusdominus/js/moment.min.js"></script>
-    <script src="lib/tempusdominus/js/moment-timezone.min.js"></script>
-    <script src="lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
-    <script src="lib/isotope/isotope.pkgd.min.js"></script>
-    <script src="lib/lightbox/js/lightbox.min.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/lib/easing/easing.min.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/lib/waypoints/waypoints.min.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/lib/owlcarousel/owl.carousel.min.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/lib/tempusdominus/js/moment.min.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/lib/tempusdominus/js/moment-timezone.min.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/lib/tempusdominus/js/tempusdominus-bootstrap-4.min.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/lib/isotope/isotope.pkgd.min.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/lib/lightbox/js/lightbox.min.js"></script>
 
     <!-- Template Javascript -->
-    <script src="js/main.js"></script>
+    <script src="<?php bloginfo('template_directory'); ?>/js/main.js"></script>
 
 	<?php wp_footer(); ?>
 
